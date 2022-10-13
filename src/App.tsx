451bf32b6834a07fff1a071753a78ae5bfc0ebe3
알파 <arpa1705@gmail.com>
2022-10-13 23:22:51 +0900

    feat: full-screen mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomeScreen from './pages/HomeScreen'
 import LockScreen from './pages/LockScreen'
 import SplashScreen from './pages/SplashScreen'
 
+import { appWindow } from '@tauri-apps/api/window'
+
 function App() {
+  useEffect(() => {
+    const handleKeydown = async (event: KeyboardEvent) => {
+      if (event.key === 'f') {
+        const isFullscreen = await appWindow.isFullscreen()
+        await appWindow.setFullscreen(!isFullscreen)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeydown)
+    return () => {
+      window.removeEventListener('keydown', handleKeydown)
+    }
+  })
+
   return (
     <BrowserRouter>
       <Routes>
